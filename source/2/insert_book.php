8ae196882c84ea5fe34f992c6071472231dec616
Nhan Nguyen <nhan.nguyen@zalora.com>
2017-03-19 11:52:09 +0700

    Save

---
+++ source/2/insert_book.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Book-O-Rama Book Entry Results</title>
+	</head>
+	<body>
+		<h1>Book-O-Rama Book Entry Results</h1>
+		<?php
+			$databases = new PDO('mysql:host=10.0.2.2;dbname=books', 'root', '');
+		    if ($databases) {
+		        die('Could not connect');
+		    }
+		    echo 'Connected successfully';
+
+		    die('DIE');
+
+			if (!isset($_POST['ISBN']) || !isset($_POST['Author']) || !isset($_POST['Title']) || !isset($_POST['Price'])) {
+				echo "<p>You have not entered all the required details.<br />
+			Please go back and try again.</p>";
+			exit;
+			}
+			// create short variable names
+			$isbn=$_POST['ISBN'];
+			$author=$_POST['Author'];
+			$title=$_POST['Title'];
+			$price=$_POST['Price'];
+			$price = doubleval($price);
+		 	if (mysqli_connect_errno()) {
+				echo "<p>Error: Could not connect to database.<br/>
+				Please try again later.</p>";
+				exit;
+		 	}
+		 	$query = "INSERT INTO books VALUES (?, ?, ?, ?)";
+			$stmt = $db->prepare($query);
+			$stmt->bind_param('sssd', $isbn, $author, $title, $price);
+			$stmt->execute();
+			if ($stmt->affected_rows > 0) {
+				echo "<p>Book inserted into the database.</p>";
+			} else {
+				echo "<p>An error has occurred.<br/>
+				The item was not added.</p>";
+			}
+		 	$db->close();
+	 ?>
+	</body>
+</html>
---
+++ source/test_connect.php
@@ -0,0 +1,4 @@
+<?php
+echo "Test connect";
+$db = mysqli_connect('10.0.2.2','root','','books');
+echo $db;
